a5cbd86a9325b88bd5636b94643e25e99ea5de3e
Alex <djcrust@gmail.com>
2022-05-11 13:18:56 +0300

    Model Task

---
+++ lib/app/data/models/task.dart
@@ -0,0 +1,25 @@
+class Task {
+  final String title;
+  final int icon;
+  final String color;
+  final List<dynamic>? todos;
+  const Task({
+    required this.title,
+    required this.icon,
+    required this.color,
+    this.todos,
+  });
+
+  Task copyWith({
+    String? title,
+    int? icon,
+    String? color,
+    List<dynamic>? todos,
+  }) =>
+      Task(
+        title: title ?? this.title,
+        icon: icon ?? this.icon,
+        color: color ?? this.color,
+        todos: todos ?? this.todos,
+      );
+}
